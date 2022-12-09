d0f055aaabc09bbea7d1724d695bcb858d2e4143
Bram van Meurs <7422223+bcvanmeurs@users.noreply.github.com>
2022-12-09 18:38:34 +0000

    day 8 part 2

--- 2022/day8.py
+++ 2022/day8.py
@@ -12,14 +12,21 @@ height = len(lines)
 width = len(lines[0])
 visible = [[False] * height for _ in range(width)]
 
+
+def distance(h, vector):
+    return next((i for i, tree in enumerate(vector) if tree >= h), len(vector) - 1) + 1
+
+
+max_score = 0
+
 for x, y in product(range(width), range(height)):
     # trees at the boundaries are visible
+    h = lines[y][x]
+    row = lines[y]
+    column = [lines[j][x] for j in range(height)]
     if x == 0 or y == 0:
         visible[y][x] = True
     else:
-        h = lines[y][x]
-        row = lines[y]
-        column = [lines[j][x] for j in range(height)]
         if (
             h > max(row[0:x], default=-1)
             or h > max(row[x + 1 :], default=-1)
@@ -28,4 +35,14 @@ for x, y in product(range(width), range(height)):
         ):
             visible[y][x] = True
 
+    # Part 2
+    left = distance(h, row[0:x][::-1])
+    right = distance(h, row[x + 1 :])
+    up = distance(h, column[0:y][::-1])
+    down = distance(h, column[y + 1 :])
+    score = left * right * up * down
+
+    max_score = max(max_score, score)
+
 print("Part 1:", sum(sum(x) for x in visible))
+print("Part 2:", max_score)
